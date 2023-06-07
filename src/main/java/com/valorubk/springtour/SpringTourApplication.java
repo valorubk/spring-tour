823046b91535ae649800ae2336de3b4dd562ab3b
ValorUBK <949685882@qq.com>
2023-06-08 00:38:30 +0800

    first commit

---
+++ src/main/java/com/valorubk/springtour/SpringTourApplication.java
@@ -0,0 +1,13 @@
+package com.valorubk.springtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valorubk/springtour/SpringTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorubk.springtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
